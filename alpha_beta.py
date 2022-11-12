d8f38e7634d6fbab04663124fbdb43405812c5f0
Mento79 <bodyomar79@gmail.com>
2022-11-12 16:04:51 +0200

    Alpha-beta working

--- alpha_beta.py
+++ alpha_beta.py
@@ -1,5 +1,4 @@
 import math
-import copy
 import random
 import State
 
@@ -36,7 +35,7 @@ def start2_minmax(Board,depth,alpha,beta,maximing_player):
 def mini_max(Board, depth, alpha, beta,maximizing_player):
     valid_location = get_valid_locations(Board)
     if depth == 0 or len(valid_location) == 0:
-        return heuristic(), 0
+        return Board.heuristic(), 0
     if maximizing_player:
         value = -math.inf
         for col2 in valid_location:
@@ -47,19 +46,20 @@ def mini_max(Board, depth, alpha, beta,maximizing_player):
             if (new_value > value):
                 value = new_value
                 Column = col2
-            if(value>=beta):
+            alpha = max(alpha, value)
+            if(beta<=alpha):
                 break
         return value, Column
     else:
         value = math.inf
         for col2 in valid_location:
-            b_copy = State.State(None, Board, col2, 1)
+            b_copy = State.State(None, Board, col2, 0)
             # drop_piece(b_copy, get_next_open_row(Board, col2), col2, 1)
             new_value, temp = mini_max(b_copy, depth - 1,alpha, beta, True)
             if (new_value < value):
                 value = new_value
                 Column = col2
-            if(value<=alpha):
+            beta=min(beta,value)
+            if(beta<=alpha):
                 break
-            beta=max(beta,value)
         return value, Column
